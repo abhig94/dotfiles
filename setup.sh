ac5540fcd3fcdcb0dd98f67167420deba9bc039a
Abhi Gupta <abhig94@gmail.com>
2017-12-26 08:49:35 -0800

    Attempt automatic download and setup of color-themes

--- setup.sh
+++ setup.sh
@@ -9,52 +9,61 @@ SCRIPTDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 ### Emacs setup
 
 # Setup julia-emacs mode
-if [ ! -d ~/.emacsconfig/julia-emacs ]; 
-then 
-    mkdir -p ~/.emacsconfig/julia-emacs 
-    git clone https://github.com/JuliaLang/julia-emacs.git ~/.emacsconfig/julia-emacs 
- else 
-     echo "$SCRIPTNAME: emacs-julia mode already installed" 
-fi 
+if [ ! -d ~/.emacsconfig/julia-emacs ];
+then
+    mkdir -p ~/.emacsconfig/julia-emacs
+    git clone https://github.com/JuliaLang/julia-emacs.git ~/.emacsconfig/julia-emacs
+ else
+     echo "$SCRIPTNAME: emacs-julia mode already installed"
+fi
 
-
+# Color-Theme setup
+if [ ! -d ~/.emacs.d/color-theme-6.6.0 ];
+then
+    wget http://download.savannah.nongnu.org/releases/color-theme/color-theme-6.6.0.tar.gz
+    tar xz color-theme-6.6.0.tar.gz && rm color-theme-6.6.0.tar.gz
+    mkdir -p ~/.emacs.d/
+    mv color-theme-6.6.0 ~/.emacs.d/
+else
+    echo "$SCRIPTNAME: color-theme already installed"
+fi
 # ESS setup
 
-if [ ! -d ~/.emacsconfig/ESS ]; 
-then 
+if [ ! -d ~/.emacsconfig/ESS ];
+then
     mkdir -p ~/.emacsconfig/ESS
     git clone git://github.com/emacs-ess/ESS.git ~/.emacsconfig/ESS
-    
+
     # move to that directory and make all
     mydir="$( pwd)"
     cd ~/.emacsconfig/ESS
     make all
     cd $mydir
- else 
-    echo "$SCRIPTNAME: ESS already installed" 
-fi 
+ else
+    echo "$SCRIPTNAME: ESS already installed"
+fi
 
 
 
-### Bash setup 
+### Bash setup
 
-# Setup git-aware-prompt 
-if [ ! -d ~/.bash/git-aware-prompt ]; 
-then 
-    mkdir -p ~/.bash/git-aware-prompt 
-     git clone https://github.com/jimeh/git-aware-prompt.git ~/.bash/git-aware-prompt 
- else 
-     echo "$SCRIPTNAME: git-aware-prompt already installed" 
-fi 
+# Setup git-aware-prompt
+if [ ! -d ~/.bash/git-aware-prompt ];
+then
+    mkdir -p ~/.bash/git-aware-prompt
+     git clone https://github.com/jimeh/git-aware-prompt.git ~/.bash/git-aware-prompt
+ else
+     echo "$SCRIPTNAME: git-aware-prompt already installed"
+fi
 
 
 # Setup Jawn
-if [ ! -d ~/Jawn ]; 
-then 
+if [ ! -d ~/Jawn ];
+then
     mkdir -p ~/Jawn
     git clone https://github.com/MattCocci/Jawn.git ~/Jawn
- else 
-    echo "$SCRIPTNAME: Jawn already installed" 
+ else
+    echo "$SCRIPTNAME: Jawn already installed"
 fi
 
 
